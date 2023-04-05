c3f8b94a4bd6159ca4771cc2724df47ac893029d
Kamotsky Yuri <kamockii1977@gmail.com>
2023-04-05 12:20:34 +0300

    init project

---
+++ src/main/java/com/wimix/automation/App.java
@@ -0,0 +1,13 @@
+package com.wimix.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
